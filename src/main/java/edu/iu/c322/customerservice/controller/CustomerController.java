208a1aba5608919f020586cbfd755c36406e1b74
Costa, Diego Souza Gomes da Cruz <dgcosta@iu.edu>
2023-03-22 17:51:10 -0400

    findAll and create added

---
+++ src/main/java/edu/iu/c322/customerservice/controller/CustomerController.java
@@ -0,0 +1,21 @@
+package edu.iu.c322.customerservice.controller;
+
+import edu.iu.c322.customerservice.model.Customer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @GetMapping
+    public List<Customer> findAll() {
+        return null;
+    }
+
+    @PostMapping
+    public int create(@RequestBody Customer customer) {
+        return 0;
+    }
+}
